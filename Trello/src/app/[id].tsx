5611af63c567190251feaf1bef745b6fe5e3d490
David Iván Rojas Robles <dirojas@springglobal.com>
2024-09-08 16:27:38 -0500

    Update a task

--- Trello/src/app/[id].tsx
+++ Trello/src/app/[id].tsx
@@ -1,13 +1,42 @@
-import { useObject } from "@realm/react";
+import { useObject, useRealm } from "@realm/react";
 import { Stack, useLocalSearchParams } from "expo-router";
-import { View, Text } from "react-native";
+import { View, Text, TextInput } from "react-native";
 import { BSON } from "realm";
 import { Task } from "../models/Task";
+import { useState } from "react";
 
 const TaskDetails = () => {
+  const realm = useRealm();
   const { id } = useLocalSearchParams();
 
   const task = useObject<Task>(Task, new BSON.ObjectID(id as string));
+  const [taskDescription, setTaskDescription] = useState(
+    task?.description ?? ""
+  );
+  const [timer, setTimer] = useState<NodeJS.Timeout>();
+
+  const updateTaskDescription = (value: string) => {
+    if (!task) {
+      return;
+    }
+    realm.write(() => {
+      task.description = value;
+    });
+  };
+
+  const onChangeText = (value: string) => {
+    setTaskDescription(value);
+
+    if (timer !== undefined) {
+      clearTimeout(timer);
+    }
+
+    const newTimer = setTimeout(() => {
+      updateTaskDescription(value);
+    }, 500);
+
+    setTimer(newTimer);
+  };
 
   if (!task) {
     return <Text>Not found</Text>;
@@ -17,7 +46,11 @@ const TaskDetails = () => {
     <View style={{ padding: 10 }}>
       <Stack.Screen options={{ title: "Task Details" }} />
 
-      <Text style={{ color: "white", fontSize: 20 }}>{task.description}</Text>
+      <TextInput
+        value={taskDescription}
+        onChangeText={onChangeText}
+        style={{ color: "white", fontSize: 20 }}
+      />
     </View>
   );
 };
